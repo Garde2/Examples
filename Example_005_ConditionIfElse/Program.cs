e66f2c227c3cdfb63e108fc306d0590a141f18f1
«Garde2» <aairon@yandex.ru>
2022-11-27 23:36:03 +0300

    add ConditionIfElse

---
+++ Example_005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "мaша")
+{
+    Console.WriteLine("Ох уж эта Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
